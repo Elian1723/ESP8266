11c72b118611b702bb0e922de42327026c1df857
Elián Barrios <elianbarrios1707@outlook.com>
2024-10-13 18:33:22 -0600

    Agregado EntityFramework

---
+++ ESP8266/Models/ProyectoContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESP8266.Models;
+
+public partial class ProyectoContext : DbContext
+{
+    public ProyectoContext()
+    {
+    }
+
+    public ProyectoContext(DbContextOptions<ProyectoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Dispositivo> Dispositivos { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=192.168.0.123;Database=Proyecto;User=sa;Password=alumnooo21!;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dispositivo>(entity =>
+        {
+            entity.HasKey(e => e.IdDispositivo);
+
+            entity.ToTable("Dispositivo");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(200);
+            entity.Property(e => e.Observacion).HasMaxLength(150);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario);
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave).HasMaxLength(150);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
